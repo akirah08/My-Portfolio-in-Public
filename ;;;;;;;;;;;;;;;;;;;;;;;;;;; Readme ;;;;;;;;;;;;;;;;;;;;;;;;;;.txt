43f419f643aea73c3266e195b7659cb8a2e81855
NEIL ALDRIN PERRAS DANCALAN <Vladlonely.vl@gmail.com>
2022-08-06 14:06:53 +0800

    First commit

---
+++ ;;;;;;;;;;;;;;;;;;;;;;;;;;; Readme ;;;;;;;;;;;;;;;;;;;;;;;;;;.txt
@@ -0,0 +1,13 @@
+Hi There!
+
+
+
+Just double-click the index.html file to view my single-page web portfolio
+to review my info and skills in web development
+
+
+Looking forward to discussing more 
+
+
+
+Thank you & God Bless
---
+++ js/arter-plugin-elementor-assets-js-front-end-widget.js
@@ -0,0 +1,21 @@
+(function ($) {
+	"use strict";
+
+	/* Init Elementor Front Scripts */
+	$(window).on('elementor/frontend/init', function () {
+
+		// Widgets
+		elementorFrontend.hooks.addAction( 'frontend/element_ready/arter-started-section.default', function() {
+			
+		} );
+
+		// Global
+		elementorFrontend.hooks.addAction( 'frontend/element_ready/global', function( $scope ) {
+			
+		} );
+		elementorFrontend.hooks.addAction( 'frontend/element_ready/widget', function( $scope ) {
+			
+		} );
+	});
+})(jQuery);
+/*This file was exported by "Export WP Page to Static HTML" plugin which created by ReCorp (https://myrecorp.com) */
